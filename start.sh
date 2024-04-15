d1017172ca1370df632963b20d20cdf0c994a3dd
Erfan Mohammad karimi zade <karimierfan74@gmail.com>
2024-04-15 19:39:20 +0330

    [update]: changing version of docker compose in start.sh

--- start.sh
+++ start.sh
@@ -17,7 +17,7 @@ else
 
     if [[ $? -ne 0 ]]; then
 	mkdir -p ~/.docker/cli-plugins/
-	curl -SL https://github.com/docker/compose/releases/download/v2.3.3/docker-compose-linux-x86_64 -o ~/.docker/cli-plugins/docker-compose
+	curl -SL https://github.com/docker/compose/releases/download/v2.26.1/docker-compose-linux-x86_64 -o ~/.docker/cli-plugins/docker-compose
 	chmod +x ~/.docker/cli-plugins/docker-compose
     fi
 
